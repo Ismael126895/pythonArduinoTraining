99c829b6d685f2431a4bcca244255e2ed8625fdf
Ismael126895 <lenojore67@gmail.com>
2023-08-14 14:26:46 +0300

    changed texts

--- prep/potMeter.py
+++ prep/potMeter.py
@@ -4,7 +4,7 @@ from vpython import *
 arduinoData=serial.Serial('com4',115200)
 time.sleep(1)
 tube=cylinder(color=color.blue,radius=1,length=1,axis=vector(0,1,0))
-lab=label(text='5 volts',box=False,pos=vector(0,.1,0))
+lab=label(text='10k Ohm resistor',box=False,pos=vector(0,.1,0))
 while True:
     while(arduinoData.inWaiting()==0):
         pass    
