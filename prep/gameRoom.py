813815643918a5c7227209c7b21258f97062f0e1
Ismael126895 <lenojore67@gmail.com>
2023-08-23 21:02:05 +0300

    updated gameroom

--- prep/gameRoom.py
+++ prep/gameRoom.py
@@ -2,39 +2,52 @@ import numpy as np
 from vpython import *
 
 
-wall_length=10
-wall_width=10
-wall_height=10
-wall_thickness=wall_length*0.01
-ball_radius=wall_thickness*5
-displacement=5
-left_wall=box(pos=vector(-displacement,0,0),
-                color=color.cyan,
-                length=wall_thickness,
-                width=wall_width,
-                height=wall_height)
-right_wall=box(pos=vector(displacement,0,0),
-                color=color.white,
-                length=wall_thickness,
-                width=wall_width,
-                height=wall_height)
-back_wall=box(pos=vector(0,0,-displacement),
-                color=color.magenta,
-                length=wall_length,
-                width=wall_thickness,
-                height=wall_height)
-top_wall=box(pos=vector(0,displacement,0),
-                color=color.yellow,
-                length=wall_length,
-                width=wall_width,
-                height=wall_thickness)
-bottom_wall=box(pos=vector(0,-displacement,0),
-                color=color.white,
-                length=wall_length,
-                width=wall_width,
-                height=wall_thickness)
+wall_lengthX=12
+wall_heightY=10
+wall_widthZ=16
+wall_thickness=.5
+wall_color=vector(1,1,1)
+ball_color=vector(0,0,1)
+wall_opacity=.8
+front_opacity=.1
+ball_radius=.5
+
+left_wall=box(pos=vector(-wall_lengthX/2,0,0),
+                color=wall_color,
+                size=vector(wall_thickness,wall_heightY,wall_widthZ),
+                opacity=wall_opacity)
+right_wall=box(pos=vector(wall_lengthX/2,0,0),
+                color=wall_color,
+                size=vector(wall_thickness,wall_heightY,wall_widthZ),
+                opacity=wall_opacity)
+back_wall=box(pos=vector(0,0,-wall_widthZ/2),
+                size=vector(wall_lengthX,wall_heightY,wall_thickness),
+                color=wall_color,
+                opacity=wall_opacity)
+front_wall=box(pos=vector(0,0,wall_widthZ/2),
+                size=vector(wall_lengthX,wall_heightY,wall_thickness),
+                color=wall_color,
+                opacity=front_opacity)
+ceiling_wall=box(pos=vector(0,wall_heightY/2,0),
+                size=vector(wall_lengthX,wall_thickness,wall_widthZ),
+                opacity=wall_opacity,
+                color=wall_color)
+floor_wall=box(pos=vector(0,-wall_heightY/2,0),
+                size=vector(wall_lengthX,wall_thickness,wall_widthZ),
+                color=wall_color,
+                opacity=wall_opacity)
 ball=sphere(radius=ball_radius,
-                color=color.red)
+                color=ball_color)
+
+ballX=0
+deltaX=.1
 
 while True:
+    rate(20) 
+    ballX=ballX+deltaX
+    if ballX+ball_radius > (wall_lengthX/2-wall_thickness/2) or ballX-ball_radius < (-wall_lengthX/2+wall_thickness/2):
+        deltaX=deltaX*(-1)
+        ballX=ballX+deltaX
+
+    ball.pos=vector(ballX,0,0)
     pass
